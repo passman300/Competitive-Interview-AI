9b1e9d3d211d1dc0741d157609f6d023a362f9a8
Colin Wang <34583138+passman300@users.noreply.github.com>
2024-12-21 14:12:37 -0500

    🔧 backend: initial setup
    - Added main server file and dependencies
    - Set up .gitignore and requirements

---
+++ backend/main.py
@@ -0,0 +1,237 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Form, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import json
+import asyncio
+
+from server import Server
+
+from pydantic import BaseModel
+from pydub import AudioSegment
+from io import BytesIO
+import speech_recognition as sr
+import openai
+
+app = FastAPI()
+
+asyncQueue = asyncio.Queue()
+
+server = Server(asyncQueue=asyncQueue)
+
+# CORS middleware to allow React app to communicate with FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # React app origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Manage rooms and connections
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: dict = {}
+
+    async def connect(self, websocket: WebSocket, room: str):
+        await websocket.accept()
+        if room not in self.active_connections:
+            self.active_connections[room] = []
+        self.active_connections[room].append(websocket)
+
+    def disconnect(self, websocket: WebSocket, room: str):
+        if room in self.active_connections:
+            self.active_connections[room].remove(websocket)
+            if not self.active_connections[room]:
+                del self.active_connections[room]
+
+    async def send_message(self, message: str, room: str, client_id: str = None):
+        # print(self.active_connections.get("a"))
+
+        json_message = json.dumps(message)
+
+        if room in self.active_connections:
+            if client_id:
+                if client_id in self.active_connections[room]:
+                    await self.active_connections[room][client_id].send_text(json_message)
+            else:
+                for connection in self.active_connections[room]:
+                    print("sending to connection!")
+                    await connection.send_text(json_message)
+
+manager = ConnectionManager()
+
+async def recv_txt(websocket: WebSocket, room: str):
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message_data = json.loads(data)
+
+            #print(message_data)
+
+            await asyncQueue.put((message_data, room))
+    except WebSocketDisconnect:
+        manager.disconnect(websocket, room)
+
+async def recv_bin(websocket: WebSocket, room: str):
+    try:
+        while True:
+            binary_data = await websocket.receive_bytes()
+            #print(f"Received binary data of length {len(binary_data)}")
+
+            with open("received_image.png", "wb") as f:
+                f.write(binary_data)
+
+            # print(f"Image saved at: {"received_image.png"}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket, room)
+
+
+@app.websocket("/ws/{room}")
+async def websocket_endpoint(websocket: WebSocket, room: str):
+    await manager.connect(websocket, room)
+
+    try:
+        while True:
+            message = await websocket.receive()
+            if message["type"] != "websocket.receive":
+                continue
+            if "bytes" in message:
+                binary_data = message["bytes"]
+
+                #print(f"Received binary data of length {len(binary_data)}")
+
+                with open("received_image.png", "wb") as f:
+                    f.write(binary_data)
+                
+                await asyncQueue.put(({
+                    "client_id": 0,
+                    "message": binary_data,
+                    "type": "frame",
+                }, room))
+
+                # print("adding to async queue")
+
+            elif "text" in message and message["text"]:
+                text_data = message["text"]
+
+                message_data = json.loads(text_data)
+
+                # print("message data", message_data)
+
+                await asyncQueue.put((message_data, room))
+                # print(f"Received text data: {text_data}")
+                await manager.send_message("Text received", websocket)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket, room)
+        print("Client disconnected.")
+    # try:
+    #     while True:
+    #         data = await websocket.receive_text()
+    #         message_data = json.loads(data)
+
+    #         print(message_data)
+
+    #         await asyncQueue.put((message_data, room))
+    # except WebSocketDisconnect:
+    #     manager.disconnect(room)
+
+@app.post("/create/{room}")
+async def create_room(room: str, company: str = "Shopify", position: str = "Software Engineer", room_name: str = "", name: str = "Guest", max_people: int = 2, max_questions: int = 5):
+    client_id = await asyncio.create_task(server.open_room(room, company, position, room_name, name, max_people, max_questions))
+    return {
+        "room": await server.get_room(room),
+        "client_id": client_id
+    }
+
+@app.get("/join/{room}")
+async def join_room(room: str, name: str = "Guest"):
+    client_id = await server.join_room(room, name)
+    return {
+        "room": await server.get_room(room),
+        "client_id": client_id
+    }
+
+class QuestionItem(BaseModel):
+    company: str
+    position: str
+    n: int
+
+@app.post("/questions")
+async def questions(item: QuestionItem):
+
+    print("Getting all contents")
+    all_contents = server.get_contents(item.company, item.position)
+
+    print("All contents fetched")
+
+    output = server.gen_questions(item.company, item.position, item.n, '\n\n'.join(all_contents))
+
+    print("Got output", output)
+
+    # json return
+    return output
+
+# class GradeItem(BaseModel):
+#     company: str
+#     position: str
+#     summary: str
+#     question: str
+#     response: str
+
+@app.post("/grade")
+async def grade(
+    summary: str = Form(...),
+    company: str = Form(...),
+    position: str = Form(...),
+    question: str = Form(...),
+    response: UploadFile = File(...),
+):
+    contents = await response.read()
+
+    with open("received_audio.wav", "wb") as audio_file:
+        audio_file.write(contents)
+
+    wav_io = BytesIO(contents)
+
+    # recognizer = sr.Recognizer()
+    # text = ""
+
+    # with sr.AudioFile(wav_io) as audio_file:
+    #     audio_data = recognizer.record(audio_file)
+    #     try:
+    #         text = recognizer.recognize_google(audio_data)
+    #     except sr.UnknownValueError:
+    #         text = "Speech not recognized"
+    #     except sr.RequestError as e:
+    #         text = f"Could not request results; {e}"
+    
+    # print("Text", text)
+
+    try:
+        wav_io.seek(0)
+        wav_io.name = "response.wav"
+        # audio_file = open("received_audio.wav", "rb")
+        transcription = openai.audio.transcriptions.create(
+            model="whisper-1",  # Choose the appropriate model, e.g., "whisper-1"
+            file=wav_io,
+            response_format="text"  # To get just the text response
+        )
+        text = transcription
+    except Exception as e:
+        text = f"An error occurred: {str(e)}"
+
+    print("transcription:", text)
+
+    output = server.gpt.score(company, position, summary, question, text)
+
+    output["transcription"] = text
+    # score is a string
+    return output
+
+server.set_manager(manager)
+
+async def startup_event():
+    asyncio.create_task(server.run())
+
+app.add_event_handler("startup", startup_event)
---
+++ backend/server.py
@@ -0,0 +1,82 @@
+import os
+from openai import OpenAI
+
+import cv2
+import numpy as np
+
+from scraper import Scraper
+from gpt import Gpt
+from camera import check_posture
+
+import asyncio
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class Server:
+    def __init__(self, asyncQueue: asyncio.Queue):
+        self.asyncQueue = asyncQueue
+
+        self.client = OpenAI(
+            api_key = os.environ.get("OPENAI_API_KEY"),
+            #organization = os.environ.get("OPENAI_ORGANIZATION")
+        )
+
+        self.scraper = Scraper()
+        self.gpt = Gpt(self.client)
+
+    def set_manager(self, manager):
+        self.manager = manager
+
+    def get_contents(self, company, position):
+        return self.scraper.get_contents(company, position)
+    
+    def gen_questions(self, company, position, n, all_contents):
+        return self.gpt.gen_questions(company, position, n, all_contents)
+
+    async def run(self):
+        print("running")
+        
+        while (True):
+            message_data, room = await self.asyncQueue.get()
+
+            if message_data is None:
+                continue
+
+            # print("message_data", message_data, room)
+
+            # parse message
+            client_id = message_data.get("client_id")
+            message = message_data.get("message")
+            type = message_data.get("type")
+
+            json_message = {
+                "client_id": client_id,
+                "type": type,
+                "message": message,
+            }
+
+            # HAVE TYPE STUFF HERE
+            if (type == "answer"):
+                print(message_data)
+
+                # handle
+                print("ANSWER RECEIVED FOR " + client_id + " aka " + message_data.get("message").get("name"))
+                
+                await self.manager.send_message(message_data, room)
+            elif (type == "frame"):
+                # print("hi")
+                frame = cv2.imdecode(np.frombuffer(message, np.uint8), cv2.IMREAD_COLOR)
+                score = check_posture(frame)
+                broadcast_message = f"Posture {score[0]} {score[1]}"
+                # print("Posture score", score)
+                #await self.manager.send_message(broadcast_message, room)
+            else:
+                print("TYPE IS " + type)
+                await self.manager.send_message(json_message, room)
+
+            # print(message)
+
+            # Format the broadcast message with client ID
+            # broadcast_message = f"Client {client_id} says: {message}"
+            # await self.manager.send_message(broadcast_message, room)
